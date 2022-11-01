d3e836bfcc0df12d082e3833bf8853e803279c3e
Jessica <aleejess@gmail.com>
2022-11-01 18:45:47 -0500

    feat:add info

--- defaultParams.js
+++ defaultParams.js
@@ -1,1 +1,7 @@
-let hola = (name= "Jess") => "hola" +name;
+// let hola = (name= "Jess") => "hola " +name;
+// console.log(hola())
+
+let getName = () => "Jess"
+let hello = (name = getName(), message = "hola " + name) => message;
+
+console.log(hello())
--- herencia.js
+++ herencia.js
@@ -30,3 +30,48 @@
 const arr = [1, 2, 3, 4]
 const sum = arr.reduce((acc, elem) => acc + elem);
 console.log(sum);
+
+const input = "Jessica Paola Martinez Alee"
+const response = input.toLowerCase().split('').reduce((acc, char)=> {
+  acc[char]? acc[char] ++ : acc[char] = 1;
+  return acc;
+}, [])
+
+console.log(response)
+
+const anonymousFn = function() {}
+
+function hello(name) {
+  return function() {
+    console.log(`Hola ${name}`)
+  }
+}
+
+const helloMaria = hello("Maria")
+helloMaria() // "Hola Maria"
+
+// o más compacto
+hello("Pedro")() 
+
+const str = "Este string es inmutable"
+const str2 = str.slice(5, 11)
+const str3 = str2.toUpperCase()
+
+console.log(str) 
+console.log(str2) 
+console.log(str3)
+
+const obj = { a: 1, b: 2, c: 3 }
+const newObj = { ...obj, c: 4, d: 5}
+// newObj queda { a: 1, b: 2, c: 4, d: 5}
+
+const person = {
+  name: "Pedro",
+  greet() {
+    console.log(`Hola ${this.name}`)
+  }
+}
+
+const hello = person.greet
+hello() // "Hola undefined"
+
